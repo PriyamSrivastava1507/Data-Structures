6d2f49a38132684ede277143c3dce4f179a64d9d
Priyam Srivastava <157367170+PriyamSrivastava1507@users.noreply.github.com>
2026-01-23 23:35:15 +0530

    Create longestNonRepeatSubstring.cpp

---
+++ Array/longestNonRepeatSubstring.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lengthOfLongestSubstring(string s) {
+    int n = s.size();
+    unordered_set<char> hashSet;
+    int maxLen = 0;
+    int i = 0, j = 0;
+
+    while (j < n) {
+        if (hashSet.find(s[j]) == hashSet.end()) {
+            hashSet.insert(s[j]);
+            maxLen = max(maxLen, j - i + 1);
+            j++;
+        } else {
+            hashSet.erase(s[i]);
+            i++;
+        }
+    }
+
+    return maxLen;
+}
+
+int main() {
+    string s1 = "abcabcbb";
+    cout << lengthOfLongestSubstring(s1) << endl;
+
+    string s2 = "pwwkew";
+    cout << lengthOfLongestSubstring(s2) << endl;
+
+    return 0;
+}
